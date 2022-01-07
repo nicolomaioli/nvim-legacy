203a2b355e916eac5f563ea49babfa6b607e551a
Nicolo Maioli <nicolomaioli@gmail.com>
2022-01-07 11:25:21 +0000

    Fix undefined global getWords

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -19,7 +19,7 @@ require("lualine").setup({
         lualine_c = {
             {"diagnostics", sources = {"nvim_diagnostic"}}, function()
                 return "%="
-            end, "filename", {getWords}
+            end, "filename"
         },
         lualine_x = {"filetype"},
         lualine_y = {{"progress"}},
